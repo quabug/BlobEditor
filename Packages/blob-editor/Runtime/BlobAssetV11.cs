2d52ad664f4e44c2d6f6669b1748cec8a84c36a4
quabug <quabug@gmail.com>
2021-08-18 18:11:06 +0800

    backward compatibility of version 1.1

---
+++ Packages/blob-editor/Runtime/BlobAssetV11.cs
@@ -0,0 +1,33 @@
+using System;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace Blob
+{
+    [Serializable, Obsolete("for backward compatibility of version 1.1 only")]
+    public class BlobAssetV11<T> where T : unmanaged
+    {
+        public BlobDataBuilder<T> Builder;
+
+        BlobAssetReference<T> _blobAssetReference;
+
+        public BlobAssetReference<T> Reference
+        {
+            get
+            {
+                if (!_blobAssetReference.IsCreated) _blobAssetReference = Create();
+                return _blobAssetReference;
+            }
+        }
+
+        public ref T Value => ref Reference.Value;
+
+        private BlobAssetReference<T> Create()
+        {
+            using var builder = new BlobBuilder(Allocator.Temp);
+            ref var root = ref builder.ConstructRoot<T>();
+            Builder.Build(builder, ref root);
+            return builder.CreateBlobAssetReference<T>(Allocator.Persistent);
+        }
+    }
+}
