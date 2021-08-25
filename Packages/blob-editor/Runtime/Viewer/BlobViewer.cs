9dd43156b3758d3b41ef6d3e8101d1230380314f
quabug <quabug@gmail.com>
2021-08-25 21:13:29 +0800

    basic viewers

--- Packages/blob-editor/Runtime/Viewer/BlobViewer.cs
+++ Packages/blob-editor/Runtime/Viewer/BlobViewer.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.Entities;
 using UnityEngine;
 
-public class BlobViewer : MonoBehaviour
+namespace Blob
 {
-    // Start is called before the first frame update
-    void Start()
+    [Serializable]
+    public class BlobViewer
     {
-        
+        [SerializeReference, UnboxSingleProperty, UnityDrawProperty] internal IViewer Viewer;
+
+        public unsafe void View<T>(T* dataPtr) where T : unmanaged => UnsafeView(new IntPtr(dataPtr), typeof(T));
+
+        public void UnsafeView(IntPtr dataPtr, Type type)
+        {
+#if UNITY_EDITOR
+            var viewerType = type.FindViewerType();
+            if (Viewer == null || Viewer.GetType() != viewerType)
+                Viewer = (IViewer) Activator.CreateInstance(viewerType);
+            Viewer.View(dataPtr, type);
+#endif
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static class BlobViewerExtension
     {
-        
+        public static unsafe void View<T>(this BlobViewer viewer, BlobAssetReference<T> blobAssetReference) where T : unmanaged
+        {
+            fixed (T* ptr = &blobAssetReference.Value)
+            {
+                viewer.View(ptr);
+            }
+        }
     }
 }
